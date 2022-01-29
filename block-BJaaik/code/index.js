29e6992e86acb4f72b8bda8b3016b04c34a8cf52
RachamallaSwathi <swathirachamalla77@gmail.com>
2022-01-29 05:44:59 -0800

    class and inheritance

---
+++ block-BJaaik/code/index.js
@@ -0,0 +1,55 @@
+//person class
+class Person{
+    constructor(name,age,gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+    eat(){
+       
+    }
+    sleep(){
+        
+    }
+    walk(){
+      
+    }
+
+}
+class Player extends Person{
+    constructor(sportsName,name,age,gender){
+        super(name,age,gender)
+        this.sportsName=sportsName;
+    }
+    play(){
+        
+    }  
+}
+
+class Teacher extends Person{
+    constructor(instituteName,name,age,gender){
+        super(name,age,gender)
+        this.instituteName=instituteName;
+    }
+   teach(){
+        
+    }  
+}
+class Artist extends Person{
+    constructor(kind,name,age,gender){
+        super(name,age,gender);
+        this.kind=kind;
+    }
+   createArt(){
+        
+    }  
+}
+
+class Cricketer extends Player{
+    constructor(sportsName,teamName,name,age,gender){
+        super(sportsName,name,age,gender)
+        this.teamName=teamName;
+    }
+   playCricket(){
+   }
+}
